9ff2ce7880792e099606ad9d93f272505b420b92
Camon-fi07 <422nikitos244@mail.ru>
2023-12-10 17:22:24 +0700

    change min length of description

--- weblog_API/Dto/Community/CreateCommunityDto.cs
+++ weblog_API/Dto/Community/CreateCommunityDto.cs
@@ -7,7 +7,6 @@ public class CreateCommunityDto
     [MinLength(1)]
     [MaxLength(100)]
     public string Name { get; set; }
-    [MinLength(1)]
     [MaxLength(500)]
     public string? Description { get; set; }
     public Boolean IsClosed { get; set; }
